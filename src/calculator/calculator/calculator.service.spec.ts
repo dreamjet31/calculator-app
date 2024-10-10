0dcf9bda0e7859357faa6e9a19f2834dac92218a
dreamjet31 <jasonchan00061@gmail.com>
2024-10-10 12:12:56 -0500

    test: add unit tests for CalculatorServicve

--- src/calculator/calculator/calculator.service.spec.ts
+++ src/calculator/calculator/calculator.service.spec.ts
@@ -1,5 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { CalculatorService } from './calculator.service';
+import { CalculatorService } from '../calculator/calculator.service';
 
 describe('CalculatorService', () => {
   let service: CalculatorService;
@@ -12,7 +12,14 @@ describe('CalculatorService', () => {
     service = module.get<CalculatorService>(CalculatorService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  it('should evaluate valid expressions correctly', () => {
+    expect(service.evaluate('(1-1)*2+3*(1-3+4)+10/2')).toBe(11);
+    expect(service.evaluate('2 + 3 * 4')).toBe(14);
+    expect(service.evaluate('10 / 2 - 1')).toBe(4);
+  });
+
+  it('should throw an error for invalid expressions', () => {
+    expect(() => service.evaluate('2 +')).toThrow('Invalid expression');
+    expect(() => service.evaluate('3 * (4 +')).toThrow('Invalid expression');
   });
 });
